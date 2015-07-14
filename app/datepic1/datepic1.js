ed6815419495de82a83b6e399c4fa08c32cf4821
chaitanya edara <chaitanya.edara@aveva.com>
2015-07-14 16:50:40 +0530

    data picker was added

---
+++ app/datepic1/datepic1.js
@@ -0,0 +1,69 @@
+﻿'use strict';
+
+angular.module('myApp.datepic1', ['ngRoute','ui.bootstrap'])
+
+.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when('/datepic1', {
+		templateUrl: 'datepic1/datepic1.html',
+		controller: 'datepic1Ctrl'
+	});
+}])
+
+.controller('datepic1Ctrl', ['$scope',function ($scope) {
+
+
+	$scope.today = function () {
+		$scope.dt = new Date();
+	};
+	$scope.today();
+
+
+
+	$scope.dateOptions = {
+		formatYear: 'yy',
+		startingDay: 1
+	};
+
+	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+	$scope.format = $scope.formats[0];
+
+
+	$scope.open = function ($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+
+		$scope.opened = true;
+	};
+
+	var tomorrow = new Date();
+	tomorrow.setDate(tomorrow.getDate() + 1);
+	var afterTomorrow = new Date();
+	afterTomorrow.setDate(tomorrow.getDate() + 2);
+	$scope.events =
+   [
+	 {
+	 	date: tomorrow,
+	 	status: 'full'
+	 },
+	 {
+	 	date: afterTomorrow,
+	 	status: 'partially'
+	 }
+   ];
+	$scope.getDayClass = function (date, mode) {
+		if (mode === 'day') {
+			var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+			for (var i = 0; i < $scope.events.length; i++) {
+				var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+				if (dayToCheck === currentDay) {
+					return $scope.events[i].status;
+				}
+			}
+		}
+
+		return '';
+	};
+
+}]);
