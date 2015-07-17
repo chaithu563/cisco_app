5b1bf10c00e971fa5f2d509eceed518c39d7388f
chaitanya edara <chaitanya.eedara@gmail.com>
2015-07-17 08:03:14 +0530

    grid like functionality added

--- app/app.js
+++ app/app.js
@@ -1,17 +1,24 @@
 'use strict';
 
+var underscore = angular.module('underscore', []);
+underscore.factory('_', function () {
+    return window._; // assumes underscore has already been loaded on the page
+});
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.datepic1',
+   'myApp.datagrid',
   'myApp.version',
-  'ui.bootstrap'
+  'ui.bootstrap',
+   'underscore'
 ]).
 config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/datagrid');
     //$stateProvider.otherwise({ redirectTo: '/view1' });
 
 
---
+++ app/datagrid/datagrid.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+
+
+
+angular.module('myApp.datagrid', ['ui.router', 'underscore'])
+
+.config(['$stateProvider', function ($stateProvider) {
+    $stateProvider.state('datagrid', {
+        url: '/datagrid',
+        templateUrl: 'datagrid/datagrid.html',
+        controller: 'datagridCtrl'
+    })
+}])
+
+.controller('datagridCtrl', ['$scope','_', function ($scope,_) {
+
+    $scope.gridData = [{ id:1, name: "Moroni", age: 50  , desc : "hero of the day1"},
+                     { id: 2, name: "Tiancum", age: 43, desc: "hero of the day2" },
+                     { id: 3, name: "Jacob", age: 27, desc: "hero of the day3" },
+                     { id: 4, name: "Nephi", age: 29, desc: "hero of the day4" },
+                     { id: 5, name: "Enos", age: 34, desc: "hero of the day5" }];
+
+    $scope.addItem = function () {
+        var max = _.max($scope.gridData, function (item) {
+            return item.id;
+
+        })
+
+        var item = {id: max.id+1 , name:"", age:null , desc:"" }
+
+        $scope.gridData.push(item);
+
+    }
+
+    $scope.deleteItem = function (item) {
+        $scope.gridData=   _.filter($scope.gridData, function (eachitem) {
+            return (eachitem.id !==item)
+
+        })
+
+
+    }
+
+}]);
