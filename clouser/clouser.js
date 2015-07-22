06e66d6a9799c708107be01e2c92df51d4e0d5e1
chaitanya edara <chaitanya.edara@aveva.com>
2015-07-22 13:50:19 +0530

    clouser basic implemented

---
+++ clouser/clouser.js
@@ -0,0 +1,17 @@
+﻿
+
+var add = (function () {
+	var counter = 0;
+	return function () { return counter += 1; }
+})();
+
+
+window.onload = function () {
+	// run your script in here
+	//add();
+	//add();
+	//add();
+	add(); add();
+	document.getElementById("myData").value = add();
+}
+
