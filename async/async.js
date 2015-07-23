dd6f647665d7d39b63806e06f0c6f81930bda78b
chaitanya edara <chaitanya.edara@aveva.com>
2015-07-23 16:48:50 +0530

    promises in javascript basic

---
+++ async/async.js
@@ -0,0 +1,27 @@
+﻿function async(fn, callback) {
+	setTimeout(function () {
+		fn();
+		callback();
+	}, 0);
+}
+function sync(fn) {
+	fn();
+}
+function foo() {
+	console.log('foo');
+}
+
+console.log('SYNC TEST --------------');
+//synchronous
+console.log('start sync');
+sync(foo);
+console.log('end sync');
+console.log('');
+console.log('ASYNC TEST --------------');
+//asynchronous
+console.log('start async');
+async(foo, function () {
+	console.log('async callback');
+});
+console.log('end async');
+//check promises in https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Promise
--- callbind/call.js
+++ callbind/call.js
@@ -28,7 +28,38 @@ myArray = [10, 2];
 myFunctionapply.apply(myObject, myArray);
 
 
+//call realtime
 
+function Product(name, price) {
+	this.name = name;
+	this.price = price;
+
+	if (price < 0) {
+		throw RangeError('Cannot create product ' +
+						  this.name + ' with a negative price');
+	}
+
+	return this;
+}
+
+function Food(name, price) {
+	Product.call(this, name, price);
+	this.category = 'food';
+}
+
+Food.prototype = Object.create(Product.prototype);
+Food.prototype.constructor = Food; // Reset the constructor from Product to Food
+
+function Toy(name, price) {
+	Product.call(this, name, price);
+	this.category = 'toy';
+}
+
+Toy.prototype = Object.create(Product.prototype);
+Toy.prototype.constructor = Toy; // Reset the constructor from Product to Toy
+
+var cheese = new Food('feta', 5);
+var fun = new Toy('robot', 40);
 
 
 //bind example
