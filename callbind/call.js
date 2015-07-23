868fb95b13eac5281225ce88747375a4d55a0116
chaitanya edara <chaitanya.edara@aveva.com>
2015-07-23 14:11:05 +0530

    call apply bind added

---
+++ callbind/call.js
@@ -0,0 +1,57 @@
+﻿function validate() {
+
+	
+}
+
+//global object
+
+function myFunction() {
+	return this;
+}
+myFunction();
+//Invoking a Function with a Function Constructor
+
+
+
+
+
+//Invoking a Function with a Function Method
+function myFunctioncall(a, b) {
+	return a * b;
+}
+myObject = myFunctioncall.call(myObject, 10, 2);
+
+function myFunctionapply(a, b) {
+	return a * b;
+}
+myArray = [10, 2];
+myFunctionapply.apply(myObject, myArray);
+
+
+
+
+
+//bind example
+
+var Button = function (content) {
+	this.content = content;
+};
+Button.prototype.click = function () {
+	console.log(this.content + ' clicked');
+}
+
+var myButton = new Button('OK');
+myButton.click();
+
+var looseClick = myButton.click;
+looseClick(); // not bound, 'this' is not myButton
+
+var boundClick = myButton.click.bind(myButton);
+boundClick(); // bound, 'this' is myButton
+
+
+window.onload = function () {
+	
+
+}
+
