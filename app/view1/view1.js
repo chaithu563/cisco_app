28fa550b5addd9f543059b6d851d8425f954412f
chaitanya edara <chaitanya.eedara@gmail.com>
2015-08-31 19:08:22 +0530

    grid pagination

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,5 +1,5 @@
-'use strict';
-angular.module('myApp.view1', ['ui.router','ui.grid'])
+'use strict'
+angular.module('myApp.view1', ['ui.router','ui.grid','ui.grid.pagination'])
 
 .config(['$stateProvider', function ($stateProvider) {
 
@@ -24,26 +24,72 @@ angular.module('myApp.view1', ['ui.router','ui.grid'])
 
     //directive code Example
 
-	//ui-grid
+    //ui-grid
+    var paginationOptions = {
+        pageNumber: 1,
+        pageSize: 25,
+        sort: null
+    };
 
     $scope.gridOptions = {
+        enableFiltering: true,
+        enableColumnResize: true,
+        paginationPageSizes: [25, 50, 75],
+        paginationPageSize: 25,
+        onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+            $scope.gridApi.core.on.sortChanged($scope, function (grid, sortColumns) {
+                if (sortColumns.length == 0) {
+                    paginationOptions.sort = null;
+                } else {
+                    paginationOptions.sort = sortColumns[0].sort.direction;
+                }
+                getPage();
+            });
+            gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+                paginationOptions.pageNumber = newPage;
+                paginationOptions.pageSize = pageSize;
+                getPage();
+            });
+        }
     };
 
+    //$scope.gridOptions.columnDefs = [
+	//  { name: 'id' },
+	//  { name: 'name' },
+	//  { name: 'age' },
+	//  { name: 'gender' },
+	//  { name: 'email' },
+    //];
     $scope.gridOptions.columnDefs = [
-	  { name: 'id' },
-	  { name: 'name' },
-	  { name: 'age' },
-	  { name: 'gender' },
-	  { name: 'email' },
+	 { name: 'name' },
+      { name: 'gender', enableSorting: false },
+      { name: 'company', enableSorting: false }
+	 
     ];
     $scope.gridOptions.data = [];
-    $scope.gridOptions.onRegisterApi = function (gridApi) {
-        $scope.gridApi1 = gridApi;
+
+    var myPosts = 'http://jsonplaceholder.typicode.com/posts';
+    var myuserdet = 'https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json';
+    //$http.get(myPosts)
+    //.success(function (data) {
+    //	$scope.gridOptions.data = data;
+    //});
+
+
+    var getPage = function () {
+        var url;
+        url = 'https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json';
+
+        $http.get(url)
+        .success(function (data) {
+            $scope.gridOptions.totalItems = 100;
+            var firstRow = (paginationOptions.pageNumber - 1) * paginationOptions.pageSize;
+            $scope.gridOptions.data = data.slice(firstRow, firstRow + paginationOptions.pageSize);
+        });
     };
-    $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
-    .success(function (data) {
-    	$scope.gridOptions.data = data;
-    });
+
+    getPage();
 
 
 }]);
